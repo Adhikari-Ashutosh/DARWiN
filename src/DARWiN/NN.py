f18fef511e03662c026a5e7e82a6a9fca0c40db3
Ashutosh Adhikari <ashutosh_adhikari@yahoo.com>
2023-04-03 06:20:46 +0530

    Built An Activations choice layer
    Imagine what if the we choose more performant activation functions per layer. (And do random swaps between them...)
    I honestly don't have any idea if this will work but in theory it should right?

--- src/DARWiN/NN.py
+++ src/DARWiN/NN.py
@@ -1,11 +1,37 @@
 import numpy as np
-import random
 
+def binstep(x):
+    return 1 if x > 0 else (0 if x == 0 else -1)
+    
+def linear(x):
+    return x
+    
+def argmax(x):
+    return np.argmax(x)
+    
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+    
+def tanh(x):
+    return np.tanh(x)
+    
+def relu(x):
+    return x if x > 0 else 0
+    
+def lrelu(x):
+    return x if x>0 else 0.1 * x
+    
+def elu(x):
+    return x if x >= 0 else np.exp(x) - 1
 
+def swish(x):
+    return x * 1/(1 + np.exp(-x)) 
 class FFN:
-    def __init__(self, input_size , output_size , layers_sizes, random_state=None):
+    def __init__(self , input_size , output_size , layers_sizes , random_state=None):
+        
         self.weights = []
         self.biases = []
+        self.activations = []
         if random_state:
             np.random.seed(random_state)
 
@@ -22,67 +48,25 @@ class FFN:
             
             # Biases are added for every layer
             self.biases.append(np.random.randn(layers_sizes[i]))
+            self.activations.append(np.random.choice(range(9)))
 
-    def forward(self, x):
+    def forward(self , x):
         for i in range(len(self.weights)):
             x = np.dot(x, self.weights[i]) + self.biases[i]
-            is_output = True if i == len(self.weights) - 1 else False
-            x = self.activation(x, is_output=is_output)
+            x = self.activation(x , i)
         return x
 
-    def activation(self, x, is_output=False):
-        if is_output:
-            # Discrete output for output layer
-            return np.argmax(x)
-        else:
-            # Tanh activation for inner layers
-            return np.tanh(x)
-    def to_hex(self):
-        hex_str = ""
-        for weights, biases in zip(self.weights, self.biases):
-            # convert each weight and bias matrix to a hexadecimal string
-            weights_hex = np.array2string(weights.ravel(), separator='')[1:-1].replace(' ', '')
-            biases_hex = np.array2string(biases, separator='')[1:-1].replace(' ', '')
-            hex_str += weights_hex + biases_hex
+    def activation(self, x, i):
+        farr = [binstep , linear , argmax , sigmoid , tanh , relu , lrelu , elu , swish]
+        fn = farr[i]
+        return fn(x)
 
-        # return the concatenated hexadecimal string
-        return hex_str.encode().hex()
-    def from_hex(self, hex_str):
-        # Split the hex string into weight and bias components
-        wb_hex = hex_str.split(" ")
 
-        # Convert each component back to numpy arrays
-        weights = []
-        biases = []
-        for i in range(len(wb_hex)):
-            # Determine if this component represents a weight or bias array
-            if i % 2 == 0:
-                # Weight array
-                w_arr = np.frombuffer(bytes.fromhex(wb_hex[i]), dtype=np.float64)
-                w_arr = np.reshape(w_arr, (self.layer_sizes[i//2], self.layer_sizes[(i//2)+1]))
-                weights.append(w_arr)
-            else:
-                # Bias array
-                b_arr = np.frombuffer(bytes.fromhex(wb_hex[i]), dtype=np.float64)
-                biases.append(b_arr)
         
-        # Set the model's weights and biases to the decoded values
-        self.weights = weights
-        self.biases = biases
-    def from_hex(self, hex_str):
-        # decode the hex string
-        hex_bytes = bytes.fromhex(hex_str)
-        # split the hex string into weight and bias sub-strings
-        sub_hex_strs = [hex_bytes[i:i+8*self.layers_sizes[j]*self.layers_sizes[j+1]+8*self.layers_sizes[j]] for j in range(len(self.layers_sizes)-1)]
         
-        # convert each weight and bias sub-string to a numpy array
-        for i, sub_hex_str in enumerate(sub_hex_strs):
-            weights_hex_str = sub_hex_str[:8*self.layers_sizes[i]*self.layers_sizes[i+1]]
-            biases_hex_str = sub_hex_str[8*self.layers_sizes[i]*self.layers_sizes[i+1]:]
-            weights_flat = np.frombuffer(bytes.fromhex(weights_hex_str), dtype=np.float64)
-            biases_flat = np.frombuffer(bytes.fromhex(biases_hex_str), dtype=np.float64)
-            self.weights[i] = np.reshape(weights_flat, (self.layers_sizes[i], self.layers_sizes[i+1]))
-            self.biases[i] = np.reshape(biases_flat, (self.layers_sizes[i+1],))
+
+
+
 class NEU:
 
     def __init__(self,Is,Os,Nl,Sl,random_state) -> None:
